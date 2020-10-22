2adb1a9db895460fde0c65cc3af9bbbde1219120
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-10-22 15:04:06 -0700

    add gitignore file

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from trivia import Trivia
 
 player_one = Player(input("Player 1 Name: "))
 player_two = Player(input("Player 2 Name: "))
-
 question = Question()
+
 trivia_game = Trivia(player_one, player_two, question)
 trivia_game.ask_question()
 trivia_game.display_winner()
