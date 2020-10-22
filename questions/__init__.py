1bebbcce7ad7fe8be94e4362773e4f1745bd9bb8
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-10-21 23:34:10 -0700

    initial commit of trivia game

---
+++ questions/__init__.py
@@ -0,0 +1,6 @@
+
+
+# while len(question_dictionary) > 0:
+#     random_key = random.choice(list(question_dictionary.keys()))
+#     print(f"{len(question_dictionary)}: {random_key}\n")
+#     del question_dictionary[random_key]
