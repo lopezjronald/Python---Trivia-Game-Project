ec880704594bf2c87e525a0f0a197de3f4ad70dd
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-10-21 23:35:18 -0700

    made some refactoring changes

--- questions/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-# while len(question_dictionary) > 0:
-#     random_key = random.choice(list(question_dictionary.keys()))
-#     print(f"{len(question_dictionary)}: {random_key}\n")
-#     del question_dictionary[random_key]
